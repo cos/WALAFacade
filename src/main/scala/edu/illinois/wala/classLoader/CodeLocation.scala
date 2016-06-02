89c5faaf8288c1c4d096082e92cf7bdb45558d1b
Caius Brindescu <brindesc@oregonstate.edu>
2016-06-02 13:08:08 -0700

    Resolving line numbers for source files

--- src/main/scala/edu/illinois/wala/classLoader/CodeLocation.scala
+++ src/main/scala/edu/illinois/wala/classLoader/CodeLocation.scala
@@ -1,5 +1,6 @@
 package edu.illinois.wala.classLoader
 
+import com.ibm.wala.cast.loader.AstMethod
 import edu.illinois.wala.Facade._
 import com.ibm.wala.classLoader.ShrikeBTMethod
 import edu.illinois.wala.ssa.IRNo
@@ -7,6 +8,7 @@ import edu.illinois.wala.ssa.IRNo
 object CodeLocation {
   def apply(m: M, i: IRNo): Option[CodeLocation] = m match {
     case m: ShrikeBTMethod => ProgramCounter(m.getBytecodeIndex(i)) map { new CodeLocation(m, _) }
+    case m: AstMethod => ProgramCounter(i) map { new CodeLocation(m, _)}
     case _ => None
   }
 }
