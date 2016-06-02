5393952e8a29c0286fa372149a0225557a2cd8d4
Caius Brindescu <brindesc@oregonstate.edu>
2016-06-02 13:05:38 -0700

    Fixed a range out of bounds exception
    For the AST based analysis, it would fail when trying to
    resolve the variable name of a value. This fixes it and gives
    the correct result

--- src/main/scala/edu/illinois/wala/ssa/RichSymbolTable.scala
+++ src/main/scala/edu/illinois/wala/ssa/RichSymbolTable.scala
@@ -3,7 +3,7 @@ package edu.illinois.wala.ssa
 import com.ibm.wala.ssa.SymbolTable
 
 case class RichSymbolTable(val st: SymbolTable) extends Iterable[V] {
-  val elements = Stream.range(1, st.getMaxValueNumber + 1) map { V(_) }
+  val elements = Stream.range(0, st.getMaxValueNumber) map { V(_) }
 
   def iterator = elements.iterator
 }
